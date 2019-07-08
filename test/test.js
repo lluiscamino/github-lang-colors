15e6e83c28aa895db332c0b65534f6e1ea661f13
lluis <caminoperez.lluis@gmail.com>
2019-07-08 22:43:50 +0200

    Add tests

---
+++ test/test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const getLanguageColor = require('../index').getColor;
+
+describe('github-lang-colors', function() {
+    it('should return valid color code', function() {
+        assert.strictEqual(getLanguageColor('JavaScript'), '#f1e05a');
+        assert.strictEqual(getLanguageColor('Go'), '#00add8');
+        assert.strictEqual(getLanguageColor('C++'), '#f34b7d');
+        assert.strictEqual(getLanguageColor('Visual Basic'), '#945db7');
+    });
+
+    it('should return valid color code even if the language does not match case', function() {
+        assert.strictEqual(getLanguageColor('Javascript'), '#f1e05a');
+        assert.strictEqual(getLanguageColor('go'), '#00add8');
+        assert.strictEqual(getLanguageColor('c++'), '#f34b7d');
+        assert.strictEqual(getLanguageColor('visUal BaSic'), '#945db7');
+    });
+
+    it('should return error if language does not exist', function() {
+        assert.deepStrictEqual(getLanguageColor('non-existent lang'), new Error('Programming language not found.'));
+    });
+});
