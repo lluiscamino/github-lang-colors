1157571eda5b0bf520a94705882a106eeb2a03ca
lluis <caminoperez.lluis@gmail.com>
2019-07-10 23:39:43 +0200

    Updates

---
+++ precache-manifest.e5b0834fa3bccd34acfd71218ef0b3fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2d9508de1040cfd96e533650c9f7d0",
+    "url": "/github-lang-colors/index.html"
+  },
+  {
+    "revision": "e85dffce63c42f9d2f35",
+    "url": "/github-lang-colors/static/css/2.5e63658b.chunk.css"
+  },
+  {
+    "revision": "24856a6d4717c300e585",
+    "url": "/github-lang-colors/static/css/main.0b144647.chunk.css"
+  },
+  {
+    "revision": "e85dffce63c42f9d2f35",
+    "url": "/github-lang-colors/static/js/2.f17ce956.chunk.js"
+  },
+  {
+    "revision": "24856a6d4717c300e585",
+    "url": "/github-lang-colors/static/js/main.cd391a21.chunk.js"
+  },
+  {
+    "revision": "65707dab4378eddc9823",
+    "url": "/github-lang-colors/static/js/runtime~main.467f61bc.js"
+  }
+]);
